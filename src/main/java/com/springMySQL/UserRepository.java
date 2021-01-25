690426d5432fe3bb4ee9b43471c455e1a547fc34
shaurya22c <shauryabavishi2016@gmail.com>
2021-01-24 18:33:34 -0600

    springBoot MySql connection demo

---
+++ src/main/java/com/springMySQL/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springMySQL;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ src/test/java/com/springMySQL/SpringBootMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
